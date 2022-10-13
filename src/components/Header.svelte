a3b4c5aa2c43b06ec6f4a847faf2f79e1615e381
TheWisePigeon <zozozozeph@gmail.com>
2022-10-13 22:28:35 +0000

    feat: added bg svg

---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<script lang="ts" >
+
+</script>
+
+<div class="text-white text-center" >HomoPigeonae</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" >
+    import "../app.css"
+</script>
+
+<div class=" bg-green-400 items-center h-screen bg-no-repeat bg-cover absolute w-full"
+    style="background-image: url('bg.svg')"
+ >
+ <slot />
+
+</div>
